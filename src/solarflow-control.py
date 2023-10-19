9579a7a945342460c39df7c1a9e899f0df9869a7
Reinhard Weber <r.brandstaedter@gmail.com>
2023-10-19 17:16:26 +0200

    bug: remove duplicate panel power limit addition

--- src/solarflow-control.py
+++ src/solarflow-control.py
@@ -468,8 +468,8 @@ def limitHomeInput(client: mqtt_client):
     limit_values.append(0 if limit<0 else limit)                # to recover faster from negative demands
     limit = int(reduce(lambda a,b: a+b, limit_values)/len(limit_values))
 
-    if direct_panel_power > 0:
-        limit += direct_panel_power
+    #if direct_panel_power > 0:
+    #    limit += direct_panel_power
 
     sm = ",".join([f'{v:>4}' for v in smartmeter_values])
     lm = ",".join([f'{v:>4}' for v in limit_values])
