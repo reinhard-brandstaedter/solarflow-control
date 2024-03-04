d6938abd4d973ee52014c0e1de581152bf8ed235
Reinhard Weber <r.brandstaedter@gmail.com>
2024-03-04 12:26:28 +0100

    baseimage install

--- src/solarflow/smartmeters.py
+++ src/solarflow/smartmeters.py
@@ -18,7 +18,7 @@ class Smartmeter:
     def __init__(self, client: mqtt_client, base_topic:str, cur_accessor:str = "Power.Power_curr", total_accessor:str = "Power.Total_in", rapid_change_diff:int = 500):
         self.client = client
         self.base_topic = base_topic
-        self.power = TimewindowBuffer(minutes=1)
+        self.power = TimewindowBuffer(minutes=2)
         self.phase_values = {}
         self.cur_accessor = cur_accessor
         self.total_accessor = total_accessor
