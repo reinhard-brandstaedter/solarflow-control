d42c1ec2398854f3b272ebcbcf18f59acfc07bb6
Reinhard Weber <r.brandstaedter@gmail.com>
2024-03-04 22:56:12 +0100

    train test

--- src/solarflow/utils.py
+++ src/solarflow/utils.py
@@ -6,6 +6,7 @@ import logging
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 FORMAT = '%(asctime)s:%(levelname)s: %(message)s'
