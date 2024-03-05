f6c4ada4305e396c0fcfaa4e58bf690c03cb36a7
Reinhard Weber <r.brandstaedter@gmail.com>
2024-03-05 08:05:01 +0100

    smartmeter timewindow buffer size

--- src/solarflow/smartmeters.py
+++ src/solarflow/smartmeters.py
@@ -21,7 +21,7 @@ class Smartmeter:
     def __init__(self, client: mqtt_client, base_topic:str, cur_accessor:str = "Power.Power_curr", total_accessor:str = "Power.Total_in", rapid_change_diff:int = 500, callback = calllback, ):
         self.client = client
         self.base_topic = base_topic
-        self.power = TimewindowBuffer(minutes=2)
+        self.power = TimewindowBuffer(minutes=1)
         self.phase_values = {}
         self.cur_accessor = cur_accessor
         self.total_accessor = total_accessor
