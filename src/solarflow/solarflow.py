5890e0d9181dd1358f81aa99189c86078bcb9861
tuxianerDE <torsten.maus1988@gmail.com>
2024-03-06 10:08:38 +0100

    added morning discharge protection SoC 0

--- src/solarflow/solarflow.py
+++ src/solarflow/solarflow.py
@@ -267,6 +267,12 @@ class Solarflow:
             m = divmod(limit,30)[0]
             r = divmod(limit,30)[1]
             limit = 30 * m + 30 * (r // 15)
+        
+        # If battery SoC reaches 0% during night, it has been observed that in the morning with first light, residual energy in the batteries gets released
+        # Hub goes then into error and no charging occurs (probably deep discharge assumed by the battery).
+        # Hence setting the output limit 0 if SoC 0%
+        if self.electricLevel == 0:
+            limit = 0
 
         # Charge-Through:
         # If charge-through is enabled the hub will not provide any power if the last full state is to long ago
