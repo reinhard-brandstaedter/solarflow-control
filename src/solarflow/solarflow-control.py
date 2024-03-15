396abc0fb704d414ee133bec0a0046f5a260039a
Reinhard Weber <r.brandstaedter@gmail.com>
2024-03-15 22:15:06 +0100

    bug

--- src/solarflow/solarflow-control.py
+++ src/solarflow/solarflow-control.py
@@ -291,7 +291,7 @@ def limitHomeInput(client: mqtt_client):
 
         # if the hub's contribution (per channel) is larger than what the direct panels max is delivering (night, low light)
         # then we can open the hub to max limit and use the inverter to limit it's output (more precise)
-        if sf_contribution/inv.getNrHubChannels() > max(inv.getDirectDCPowerValues):
+        if sf_contribution/inv.getNrHubChannels() > max(inv.getDirectDCPowerValues()):
             log.info(f'Hub should contribute more ({sf_contribution:.1f}W) than what we currently get from panels ({direct_panel_power:.1f}W), we will use the inverter for fast/precise limiting!')
             hub_limit = hub.setOutputLimit(MAX_INVERTER_INPUT)
             direct_limit = sf_contribution/inv.getNrHubChannels()
